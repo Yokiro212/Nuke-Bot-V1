ed2e44bde96ebef000a1e017733924150ed7b221
Yokiro212 <110670248+Yokiro212@users.noreply.github.com>
2022-10-11 21:55:41 +0530

    Create launch.js

---
+++ launch.js
@@ -0,0 +1,8 @@
+const title = String.raw`yokiro on top`;
+
+console.log(title)
+
+console.log(``);
+console.log(`                                          Made by Yokiro`)
+console.log(``);
+console.log(``);
